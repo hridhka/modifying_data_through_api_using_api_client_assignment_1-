41de0ee0958c9c97430182c564049f4ce774fa5d
hridhika <hridhika031@gmail.com>
2025-04-04 16:02:52 +0530

    modified

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const menuRoutes = require('./routes/MenuRoutes.js');
+const menuRoutes = require('./routes/menuRoutes.js');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
